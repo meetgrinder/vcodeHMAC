ebcb270edb4cf5897df88e30634b2f8809acddcd
jconery <jconery@veracode.com>
2023-05-18 14:18:38 -0400

    parse profile name in creds file

--- credentials.go
+++ credentials.go
@@ -6,8 +6,12 @@ import (
 	"strings"
 )
 
-func getCredentials(fileString string) ([2]string, error) {
+func getCredentials(fileString string, profile string) ([2]string, error) {
 	var credentials [2]string
+    profileLine := '[' + profile + ']'
+    foundProfile := false
+    foundKey := false
+    foundSecret := false
 
 	file, err := os.Open(fileString)
 	if err != nil {
@@ -17,14 +21,37 @@ func getCredentials(fileString string) ([2]string, error) {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+        line := scanner.Text()
+        line = strings.TrimSpace(line)
+        if len(line) == 0 || line[0] == '#' {
+            // skip empty lines or lines starting with '#'
+            continue
+
+        if line == profileLine {
+            foundProfile = true
+            continue
+        }   
+
+        // dont even bother with anything until youve found the profile
+        if foundProfile == false {
+            continue
+        }
+        
+
 		//We remove spaces to account for discrepancies in user configuration of creds file
 		if strings.Contains(scanner.Text(), "veracode_api_key_id") {
 			removeSpaces := strings.Replace(scanner.Text(), " ", "", -1)
 			credentials[0] = strings.Replace(removeSpaces, "veracode_api_key_id=", "", -1)
+            foundKey = true
 		} else if strings.Contains(scanner.Text(), "veracode_api_key_secret") {
 			removeSpaces := strings.Replace(scanner.Text(), " ", "", -1)
 			credentials[1] = strings.Replace(removeSpaces, "veracode_api_key_secret=", "", -1)
+            foundSecret = true
 		}
+
+        if foundKey == true && foundSecret == true {
+            break
+        }
 	}
 
 	if err := scanner.Err(); err != nil {
--- generateHMAC.go
+++ generateHMAC.go
@@ -3,7 +3,7 @@ package vcodeHMAC
 
 // GenerateAuthHeader takes the location of your credentials file, the HTTP Method, and URL and returns the header value to be used for Authorization
 func GenerateAuthHeader(credsFile, httpMethod, url string) (string, error) {
-	credentials, err := getCredentials(credsFile)
+	credentials, err := getCredentials(credsFile, 'default')
 	if err != nil {
 		return "", err
 	}
@@ -23,3 +23,27 @@ func GenerateAuthHeader(credsFile, httpMethod, url string) (string, error) {
 	}
 	return headerValue, nil
 }
+
+// GenerateAuthHeader takes the location of your credentials file, the HTTP Method, and URL and returns the header value to be used for Authorization
+func GenerateAuthHeaderForProfile(credsFile, httpMethod, url string, profile string) (string, error) {
+    credentials, err := getCredentials(credsFile, profile)
+    if err != nil {
+        return "", err
+    }
+
+    host, err := getHost(url)
+    if err != nil {
+        return "", err
+    }
+    params, err := getPathParams(url)
+    if err != nil {
+        return "", err
+    }
+
+    headerValue, err := generateHeader(host, params, httpMethod, credentials[0], credentials[1], defaultAuthScheme)
+    if err != nil {
+        return "", err
+    }
+    return headerValue, nil
+}
+
