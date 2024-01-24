a93f9e11df8ba343f607e2c3fdc4b62a09266520
Sam Nelson <snelson@veracode.com>
2024-01-24 09:50:26 -0600

    added code to split the port from the host

--- utils.go
+++ utils.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"net"
 	"net/url"
 	"time"
 )
@@ -24,7 +25,8 @@ func getHost(urlString string) (string, error) {
 		return "", err
 	}
 
-	return u.Host, nil
+	host, _, err := net.SplitHostPort(u.Host)
+	return host, nil
 }
 
 func getPathParams(urlString string) (string, error) {
